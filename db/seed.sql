1936c8b869e77fc4de82669a5af073ea84468830
Karen Astell <KarenAstell@gmail.com>
2020-06-16 13:13:23 -0400

    added seed data

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,14 @@
-USE employees;
-
 INSERT INTO employees
 (first_name, last_name)
 VALUES
 ("Karen", "Astell"), ("Heidi", "Katz"), ("Kaleigh", "Spurio"), ("Brian", "Astell");
+
+INSERT INTO roles
+(title, salary)
+VALUES
+("Software Engineer", 70000), ("Lead Engineer", 100000), ("Lawyer", 120000), ("Salesperson", 85000);
+
+INSERT INTO departments
+(name)
+VALUES
+("Engineering"), ("Engineering"), ("Legal"), ("Sales");
